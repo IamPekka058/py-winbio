8ede61d0bc571fd36bdd98b6b34c09945df09bd5
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2022-07-09 19:00:12 +0200

    Added new tests

--- src/py_winbio/test.py
+++ src/py_winbio/test.py
@@ -1,3 +1,7 @@
 from WinBioAuth import WinBioAuthenticator
 
 authenticator = WinBioAuthenticator()
+size = authenticator.enumerateBiometricUnits().response
+print(size)
+if(size > 0):
+    authenticator.identify()
