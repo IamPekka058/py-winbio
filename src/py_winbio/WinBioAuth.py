2949e7caf34df64bcf8aab353698d8c0373512cf
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2023-07-14 23:19:31 +0200

    locateSensor() and further debug
    Introduced locateSensor()
    Introduced further debug

--- src/py_winbio/WinBioAuth.py
+++ src/py_winbio/WinBioAuth.py
@@ -7,6 +7,7 @@ class WinBioAuthenticator():
         self.lib = ctypes.CDLL(lib_path)
         
         # Init variables
+        self.identity = Types.WINBIO_IDENTITY()
         session_handle = ctypes.c_uint32()
         self.session_handle_ptr = ctypes.pointer(session_handle)
         if(openSession == False):
@@ -16,26 +17,43 @@ class WinBioAuthenticator():
         self.openSession()
     
     def openSession(self, WINBIO_TYPE = Enum.WINBIO_TYPE.FINGERPRINT, WINBIO_POOL = Enum.WINBIO_POOL.SYSTEM, WINBIO_FLAG = Enum.WINBIO_FLAG.DEFAULT):
-        ret = self.lib.WinBioOpenSession(WINBIO_TYPE, WINBIO_POOL, WINBIO_FLAG, None, 0, None, self.session_handle_ptr)
-        
+        print("OpenSession (Session Handle) -> {}".format(self.session_handle_ptr.contents))
+        ret = self.lib.WinBioOpenSession(WINBIO_TYPE, WINBIO_POOL, WINBIO_FLAG, ctypes.c_void_p(None), 0, Enum.WINBIO_DB.DEFAULT, self.session_handle_ptr)
+        print("OpenSession (Return) -> {}".format(ret))
+        print("OpenSession (Session Handle) -> {}".format(self.session_handle_ptr.contents))
         #Check if operation failed
         failed = FAILED(ret)
+        print("OpenSession -> {}".format(failed))
         if(failed[0]):
             return Types.Result(False, failed[1])
         return Types.Result(True, "Success.")
+    
+    def locateSensor(self):
+        self.unit_id = ctypes.c_ulong()
+        ret = self.lib.WinBioLocateSensor(self.session_handle_ptr.contents, ctypes.byref(self.unit_id))
+        failed = FAILED(ret)
+        if(failed):
+            print("locateSensor (Error) -> {}".format(failed))
+            return
+        print(self.unit_id)
 
     def identify(self, subtype = Enum.WINBIO_FINGER_UNSPECIFIED.POS_01):
         unitid_ptr = ctypes.pointer(ctypes.c_ulong())
         identity_ptr = ctypes.pointer(Types.WINBIO_IDENTITY())
-        subtype_ptr = ctypes.pointer(ctypes.c_ubyte(subtype))
+        subtype_ptr = ctypes.pointer(ctypes.c_ubyte())
         reject_detail_ptr = ctypes.pointer(ctypes.c_ulong())
-        
-        ret = self.lib.WinBioIdentify(self.session_handle_ptr[0],unitid_ptr, identity_ptr, subtype_ptr, reject_detail_ptr)
-
+        print("IDENTIFY (Session Handle) -> {}".format(self.session_handle_ptr.contents))
+        print("DEBUG Library wird aufgerufen.")
+        ret = self.lib.WinBioIdentify(self.session_handle_ptr.contents,unitid_ptr, identity_ptr, subtype_ptr, reject_detail_ptr)
+        print(reject_detail_ptr)
+        print("IDENTIFY -> {}".format(ret))
+        print("DEBUG Library wurde aufgerufen.")
         failed = FAILED(ret)
+        print("IDENTIFY -> {}".format(failed))
         if(failed[0]):
             return Types.Result(False, failed[1])
         self.identity = identity_ptr[0]
+        self.free(ctypes.addressof(identity_ptr))
         return Types.Result(True, self.identity)
 
     def verify(self, subtype = Enum.WINBIO_FINGER_UNSPECIFIED.POS_01):
@@ -58,7 +76,6 @@ class WinBioAuthenticator():
         size_ptr = ctypes.pointer(size)
         
         ret = self.lib.WinBioEnumBiometricUnits(WINBIO_TYPE, schema_ptr, size_ptr)
-        
         #Check if operation failed
         failed = FAILED(ret)
         if(failed[0]):
