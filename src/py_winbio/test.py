1035f32e46081b48480021d29b47adfccdf0fec2
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2023-07-14 23:20:37 +0200

    Update test.py

--- src/py_winbio/test.py
+++ src/py_winbio/test.py
@@ -5,5 +5,8 @@ authenticator = WinBioAuthenticator()
 size = authenticator.enumerateBiometricUnits().response
 print("{0} Units found.".format(size))
 if(size > 0):
+    authenticator.locateSensor()
+    input("Identifizierung?")
     authenticator.identify()
+    input("Verifizierung?")
     authenticator.verify()
