1548d134810aa6a897c3f283e15f587451f55351
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2024-08-30 12:39:33 +0200

    Refactored test.py to avoid errors due to deactivation of certain methods

--- src/py_winbio/test.py
+++ src/py_winbio/test.py
@@ -2,15 +2,7 @@ from WinBioAuth import WinBioAuthenticator
 
 authenticator = WinBioAuthenticator()
 
-size = authenticator.enumerateBiometricUnits().response
-print("{0} Units found.".format(size))
-if(size > 0):
-    #authenticator.acquireFocus()
-    #input("Hallo?")
-    #authenticator.locateSensor()
-    #input("Identifizierung?")
-    #authenticator.identify()
-    authenticator.enrollBegin()
-    authenticator.enrollCapture()
-    input("Verifizierung?")
-    #authenticator.verify()
+size = authenticator.enumerateBiometricUnits().response['size']
+print("{0} Units found.".format(size.value))
+if size.value > 0:
+    authenticator.identify()
