02cc59872ecb5f31e0899ed2599c160a08bfcff8
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2024-08-30 12:38:40 +0200

    Code refactoring

--- src/py_winbio/Helper.py
+++ src/py_winbio/Helper.py
@@ -4,38 +4,38 @@ import Enum, Types
 def convert(hresult):
     return ctypes.c_int32(hresult).value
 
-def FAILED(hr):
-    if(hr == convert(Enum.HRESULT.E_ABORT)):
-        return (True, "E_ABORT")
+def RESULT(hr):
+    if hr == convert(Enum.HRESULT.E_ABORT):
+        return Types.Result(False, "E_ABORT")
 
-    if(hr == convert(Enum.HRESULT.E_ACCESSDENIED)):
-        return (True, "E_ACCESSDENIED")
+    if hr == convert(Enum.HRESULT.E_ACCESSDENIED):
+        return Types.Result(False, "E_ACCESSDENIED")
 
-    if(hr == convert(Enum.HRESULT.E_FAIL)):
-        return (True, "E_FAIL")
+    if hr == convert(Enum.HRESULT.E_FAIL):
+        return Types.Result(False, "E_FAIL")
 
-    if(hr == convert(Enum.HRESULT.E_HANDLE)):
-        return (True, "E_HANDLE")
+    if hr == convert(Enum.HRESULT.E_HANDLE):
+        return Types.Result(False, "E_HANDLE")
     
-    if(hr == convert(Enum.HRESULT.E_INVALIDARG)):
-        return (True, "E_INVALIDARG")
+    if hr == convert(Enum.HRESULT.E_INVALIDARG):
+        return Types.Result(False, "E_INVALIDARG")
     
-    if(hr == convert(Enum.HRESULT.E_NOINTERFACE)):
-        return (True, "E_NOINTERFACE")
+    if hr == convert(Enum.HRESULT.E_NOINTERFACE):
+        return Types.Result(False, "E_NOINTERFACE")
     
-    if(hr == convert(Enum.HRESULT.E_NOTIMPL)):
-        return (True, "E_NOTIMPL")
+    if hr == convert(Enum.HRESULT.E_NOTIMPL):
+        return Types.Result(False, "E_NOTIMPL")
 
-    if(hr == convert(Enum.HRESULT.E_OUTOFMEMORY)):
-        return (True, "E_OUTOFMEMORY")
+    if hr == convert(Enum.HRESULT.E_OUTOFMEMORY):
+        return Types.Result(False, "E_OUTOFMEMORY")
 
-    if(hr == convert(Enum.HRESULT.E_POINTER)):
-        return (True, "E_POINTER")
+    if hr == convert(Enum.HRESULT.E_POINTER):
+        return Types.Result(False, "E_POINTER")
     
-    if(hr == convert(Enum.HRESULT.E_UNEXPECTED)):
-        return (True, "E_UNEXPECTED")
+    if hr == convert(Enum.HRESULT.E_UNEXPECTED):
+        return Types.Result(False, "E_UNEXPECTED")
 
-    if(hr == convert(Enum.HRESULT.S_OK)):
-        return (False, "S_OK")
+    if hr == convert(Enum.HRESULT.S_OK):
+        return Types.Result(True, "S_OK")
     
-    return (True, "NO SPECIFIC ERROR")
+    return Types.Result(False, "NO SPECIFIC ERROR, HRESULT: {}".format(hr))
--- src/py_winbio/WinBioAuth.py
+++ src/py_winbio/WinBioAuth.py
@@ -1,64 +1,65 @@
 import ctypes, Enum, Types
 from ctypes import wintypes
-from Helper import FAILED
+from Helper import RESULT
 
 class WinBioAuthenticator():
-    
-    def __init__(self, lib_path = r"C:\Windows\System32\winbio.dll", openSession = True):
+    def __init__(self, lib_path = r"C:\Windows\System32\winbio.dll", openSession = True) -> None:
         # Setting lib
-        self.lib = ctypes.WinDLL(lib_path)
-        
+        self.lib = ctypes.CDLL(lib_path)
+
         # Init variables
-        self.identity = Types.WINBIO_IDENTITY()
-        self.session_handle = wintypes.HANDLE()
-        #self.session_handle_ptr = ctypes.pointer(session_handle)
-        if(openSession == False):
-            return None
-        
+        self.unit_ids = []
+        self.availableBiometricUnits = []
+        self.session_handle = ctypes.c_uint32()
+        self.session_handle_ptr = ctypes.pointer(self.session_handle)
+        if not openSession:
+            pass
+
         #Open session
         self.openSession()
-    
+
     def openSession(self, WINBIO_TYPE = Enum.WINBIO_TYPE.FINGERPRINT, WINBIO_POOL = Enum.WINBIO_POOL.SYSTEM, WINBIO_FLAG = Enum.WINBIO_FLAG.DEFAULT):
-        #print("OpenSession (Session Handle) -> {}".format(self.session_handle_ptr.contents))
         ret = self.lib.WinBioOpenSession(WINBIO_TYPE, WINBIO_POOL, WINBIO_FLAG, ctypes.c_void_p(None), 0, Enum.WINBIO_DB.DEFAULT, ctypes.byref(self.session_handle))
-        print("OpenSession (Return) -> {}".format(ret))
-        print("OpenSession (Session Handle) -> {}".format(self.session_handle))
+
         #Check if operation failed
-        failed = FAILED(ret)
-        print("OpenSession -> {}".format(failed))
-        if(failed[0]):
-            return Types.Result(False, failed[1])
-        return Types.Result(True, "Success.")
+        job = RESULT(ret)
+
+        if not job.state:
+            print("Failed to open session, HRESULT: ", job.response)
+            return job
+        print("Successfully opened session, HRESULT: ", job.response)
+        return job
     
     def locateSensor(self):
-        self.unit_id = wintypes.ULONG()
-        print("Wird aufgerufen...")
-        ret = self.lib.WinBioLocateSensor(self.session_handle, ctypes.byref(self.unit_id))
-        print("Aufgerufen...")
-        failed = FAILED(ret)
-        if(failed):
-            print("locateSensor (Error) -> {}".format(failed))
-            return
-        print(self.unit_id)
+        unit_id = ctypes.pointer(wintypes.ULONG())
+
+        ret = self.lib.WinBioLocateSensor(self.session_handle, unit_id)
+
+        job = RESULT(ret)
+        if not job.state:
+            print("Failed to locate sensor, HRESULT: ", job.response)
+            return job
+        print("Successfully located sensor.")
+        job.response = unit_id
+        return job
 
     def identify(self, subtype = Enum.WINBIO_FINGER_UNSPECIFIED.POS_01):
         unitid_ptr = ctypes.pointer(ctypes.c_ulong())
         identity_ptr = ctypes.pointer(Types.WINBIO_IDENTITY())
         subtype_ptr = ctypes.pointer(ctypes.c_ubyte())
         reject_detail_ptr = ctypes.pointer(ctypes.c_ulong())
-        print("IDENTIFY (Session Handle) -> {}".format(self.session_handle_ptr.contents))
-        print("DEBUG Library wird aufgerufen.")
+
+        print("Starting Identification Process...")
+
         ret = self.lib.WinBioIdentify(self.session_handle_ptr.contents,unitid_ptr, identity_ptr, subtype_ptr, reject_detail_ptr)
-        print(reject_detail_ptr)
-        print("IDENTIFY -> {}".format(ret))
-        print("DEBUG Library wurde aufgerufen.")
-        failed = FAILED(ret)
-        print("IDENTIFY -> {}".format(failed))
-        if(failed[0]):
-            return Types.Result(False, failed[1])
-        self.identity = identity_ptr[0]
+
+        job = RESULT(ret)
+        print("IDENTIFY -> {}".format(job))
+        if not job.state:
+            return job
+        job.response = identity_ptr[0]
         self.free(ctypes.addressof(identity_ptr))
-        return Types.Result(True, self.identity)
+        return job
 
     def verify(self, subtype = Enum.WINBIO_FINGER_UNSPECIFIED.POS_01):
         identity_ptr = ctypes.pointer(self.identity)
@@ -69,80 +70,96 @@ class WinBioAuthenticator():
 
         ret = self.lib.WinBioVerify(self.session_handle_ptr[0], subtype, identity_ptr, unitId_ptr, match_ptr, reject_detail_ptr)
 
-        failed = FAILED(ret)
-        if(failed[0]):
-            return Types.Result(False, failed[1])
-        return Types.Result(True, self.failed[1])
+        job = RESULT(ret)
+        if not job.state:
+            print("Failed to verify, HRESULT: ", job.response)
+            return job
+        print("Successfully verified, HRESULT: ", job.response)
+        return job
 
     def enumerateBiometricUnits(self, WINBIO_TYPE = Enum.WINBIO_TYPE.FINGERPRINT):
         schema_ptr = ctypes.pointer(Types.WINBIO_UNIT_SCHEMA())
+        schema_ptr_array = ctypes.pointer(schema_ptr)
+
         size = ctypes.c_int32()
         size_ptr = ctypes.pointer(size)
-        schema_ptr_ptr = ctypes.pointer(schema_ptr)
-        ret = self.lib.WinBioEnumBiometricUnits(WINBIO_TYPE, schema_ptr_ptr, size_ptr)
-        print(size_ptr.contents)
-        self.unit_id = schema_ptr.contents.UnitId
-        #Check if operation failed
-        self.free(schema_ptr_ptr.contents)
-        failed = FAILED(ret)
-        if(failed[0]):
-            return Types.Result(False, failed[1])
-        self.availableBiometricUnits = schema_ptr[0]
-        return Types.Result(True, size_ptr[0])
+
+        ret = self.lib.WinBioEnumBiometricUnits(WINBIO_TYPE, schema_ptr_array, size_ptr)
+
+        self.unit_ids = schema_ptr_array
+
+        if schema_ptr_array.contents is not None:
+            # Secure array list before free/delete
+            self.availableBiometricUnits = schema_ptr[0]
+            self.free(schema_ptr)
+            schema_ptr = None
+
+        job = RESULT(ret)
+
+        if not job.state:
+            print("Failed to enumerate Biometric Units, HRESULT: ", job.response)
+            return -1
+        print("Successfully enumerated Biometric Units, HRESULT: ", job.response)
+        job.response = {"size": size, "schema": schema_ptr}
+        return job
 
     def cancel(self):
         ret = self.lib.WinBioCancel(self.session_handle_ptr[0])
 
-        failed = FAILED(ret)
-        if(failed[0]):
-            return Types.Result(False, failed[1])
-        return Types.Result(True, failed[1])
+        job = RESULT(ret)
+        if not job.state:
+            return job
+        return job
 
     def free(self, address):
         ret = self.lib.WinBioFree(address)
 
-        failed = FAILED(ret)
-        if(failed[0]):
-            return Types.Result(False, failed[1])
-        return Types.Result(True, failed[1])
-    
-    def acquireFocus(self):
-        ret = self.lib.WinBioAcquireFocus()
-        
-        failed = FAILED(ret)
-        if(failed[0]):
-            return Types.Result(False, failed[1])
-        return Types.Result(True, failed[1])
-    
-    def releaseFocus(self):
-        ret = self.lib.WinBioRealeaseFocus()
-        
-        failed = FAILED(ret)
-        if(failed[0]):
-            return Types.Result(False, failed[1])
-        return Types.Result(True, failed[1])
-    
-    def enrollBegin(self, subfactor = Enum.SUBFACTOR.WINBIO_ANSI_381_POS_LH_INDEX_FINGER):
-        ret = self.lib.WinBioEnrollBegin(self.session_handle, self.unit_id)
-
-        failed = FAILED(ret)
-        if(failed[0]):
-            return Types.Result(False, failed[1])
-        return Types.Result(True, failed[1])
-
-    def enrollCapture(self):
-        rejectDetail = ctypes.c_ulong()
-        ret = self.lib.WinBioEnrollCapture(self.session_handle, ctypes.byref(rejectDetail))
-        print(ret)
-        print(rejectDetail)
-        failed = FAILED(ret)
-        if(failed[0]):
-            return Types.Result(False, failed[1])
-        return Types.Result(True, failed[1])
-
-
-
-
+        job = RESULT(ret)
+        if not job.state:
+            return job
+        return job
+# Following methods need local system permissions on windows
+# Currently deactivated
+#    def acquireFocus(self):
+#        ret = self.lib.WinBioAcquireFocus()
+#        failed = FAILED(ret)
+#        if(failed[0]):
+#            print("Failed to acquire focus, HRESULT: ", failed[1])
+#            return Types.Result(False, failed[1])
+#        print("Acquire focus, HRESULT: ", hex(ret))
+#        return Types.Result(True, failed[1])
+#
+#    def releaseFocus(self):
+#        ret = self.lib.WinBioRealeaseFocus()
+#
+#        failed = FAILED(ret)
+#        if(failed[0]):
+#            return Types.Result(False, failed[1])
+#        return Types.Result(True, failed[1])
+#
+#    def enrollBegin(self, subfactor = Enum.SUBFACTOR.WINBIO_ANSI_381_POS_LH_INDEX_FINGER):
+#        ret = self.lib.WinBioEnrollBegin(self.session_handle, self.unit_id)
+#
+#        failed = FAILED(ret)
+#        if(failed[0]):
+#            return Types.Result(False, failed[1])
+#        return Types.Result(True, failed[1])
+#
+#    def enrollCapture(self):
+#        rejectDetail = ctypes.c_ulong()
+#        ret = self.lib.WinBioEnrollCapture(self.session_handle, ctypes.byref(rejectDetail))
+#        print(ret)
+#        print(rejectDetail)
+#        failed = FAILED(ret)
+#        if(failed[0]):
+#            return Types.Result(False, failed[1])
+#        return Types.Result(True, failed[1])
+#
+#
+#
+#
+#        print("Successfully enumerated Biometric Units,  HRESULT: ", hex(ret))
+#        return size_ptr[0]
 
 
 
